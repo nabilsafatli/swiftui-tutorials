477c9fbbe7cd3c9ef000605cbb96ba693eb86d81
nabil.safatli <safatli.nabil@gmail.com>
2020-02-06 15:28:56 +0100

    Complete 3 Sections of Swift UI tutorials

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nabil Safatli on 2020-02-06.
+//  Copyright © 2020 Nabil Safatli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
