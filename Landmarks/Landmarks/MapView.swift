1f5bf466f71032cf6549c891c6f396ce8f9ed86f
nabil.safatli <safatli.nabil@gmail.com>
2020-02-06 16:08:07 +0100

    Refactor type of return to Context

--- Landmarks/Landmarks/MapView.swift
+++ Landmarks/Landmarks/MapView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
-    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MapView.UIViewType {
+    func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
-    
-    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
-        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
